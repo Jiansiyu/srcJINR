3cd2d52dcb213de9ac3926a24ea4665dd1497987
segarrae <segarrae@eofe7.cm.cluster>
2018-05-01 14:21:08 -0400

    Initalized src repo

--- SetEnv.sh
+++ SetEnv.sh
@@ -3,9 +3,9 @@
 SIMMACHINE=$(uname -n)
 
 # change SIMPATH if you installed FairSoft not to the '/opt/fairsoft/install' directory
-export SIMPATH=/opt/fairsoft/install
+export SIMPATH=/nobackup1/segarrae/software/fairsoft
 # change FAIRROOTPATH if you installed FairRoot not to the '/opt/fairroot/install' directory
-export FAIRROOTPATH=/opt/fairroot/install
+export FAIRROOTPATH=/nobackup1/segarrae/software/fairroot
 
 export ROOTSYS=$SIMPATH
 
