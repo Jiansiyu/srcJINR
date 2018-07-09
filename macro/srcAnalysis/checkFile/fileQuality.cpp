48dcfd8fb68df175a6dddc867265de26ce2c0325
Efrain Segarra <segarrae@mit.edu>
2018-07-09 16:45:02 -0400

    Made way better quality control check file in order to look at ToF400 runs

--- macro/srcAnalysis/checkFile/fileQuality.cpp
+++ macro/srcAnalysis/checkFile/fileQuality.cpp
@@ -9,16 +9,22 @@
 #include "TTree.h"
 #include "TClonesArray.h"
 #include "TH1D.h"
+#include "TH2D.h"
+#include "TF1.h"
 #include "TFitResult.h"
 #include "TFitResultPtr.h"
+#include "TApplication.h"
+#include "TCanvas.h"
+#include "TGraph.h"
+#include "TGraphErrors.h"
+#include "TStyle.h"
+#include "Rtypes.h"
+#include "TVectorT.h"
 
 #include "BmnTrigWaveDigit.h"
 #include "BmnTrigDigit.h"
 
 using namespace std;
-void findIdx( TClonesArray* data, int &index , double refT);
-void fillPedestal( TH1D* hist, TClonesArray* data, int index);
-
 
 int main(int argc, char ** argv)
 {
@@ -26,240 +32,283 @@ int main(int argc, char ** argv)
 	if (argc !=2)
 	{
 		cerr << "Wrong number of arguments. Instead use\n"
-			<< "\tfileQuality /path/to/digi/file\n";
+			<< "\tfileQuality checkList.txt\n";
 		return -1;
 	}
 
-	TString file = argv[1];
-	TString run_number( file(file.Index(".")-9,4) );
+
 	
 	// Attempt to find a matchin pedestal file:
-	TFile * infile_ped = NULL;
-	TString path = std::getenv("VMCWORKDIR");
-	path = path + "/build/bin/qualityCheck/tqdcPedestals" + run_number + ".root";
-	infile_ped = new TFile(path);
-	if (!infile_ped){
-		cerr << "Could not open file " << path <<"\n"
-			<< "\tBailing out\n";
-		return -2;
-	}
-	else{
-		//cerr << "Successfully opened file " << path << " and saved it to address " << infile_ped << "\n";
-	}
+	string fileName = string(argv[1]);
+	ifstream list (fileName);
+	string run_number;
+
+	std::vector<double> fileNo;
+	std::vector<double> rate_t0;
+	std::vector<double> pedestals_bc1;
+	std::vector<double> pedestals_bc2;
+	std::vector<double> pedestals_bc3;
+	std::vector<double> pedestals_bc4;
+	std::vector<double> tdiff_bc1;
+	std::vector<double> tdiff_bc2;
+	std::vector<double> tdiff_bc3;
+	std::vector<double> tdiff_bc4;
+	std::vector<double> tres_bc1;
+	std::vector<double> tres_bc2;
+	std::vector<double> tres_bc3;
+	std::vector<double> tres_bc4;
+	std::vector<double> tped_bc1;
+	std::vector<double> tped_bc2;
+	std::vector<double> tped_bc3;
+	std::vector<double> tped_bc4;
+	std::vector<double> tdc_bc1;
+	std::vector<double> tdc_bc2;
+	std::vector<double> tdc_bc3;
+	std::vector<double> tdc_bc4;
+	std::vector<double> carbon_bc1;
+	std::vector<double> carbon_bc2;
+	std::vector<double> carbonWi_bc1;
+	std::vector<double> carbonWi_bc2;
 
 
-	// Read in pedestals
-	TH1D * BC1_ped = (TH1D * ) infile_ped->Get("BC1_ped");
-	TH1D * BC2_ped = (TH1D * ) infile_ped->Get("BC2_ped");
-	TH1D * BC3_ped = (TH1D * ) infile_ped->Get("BC3_ped");
-	TH1D * BC4_ped = (TH1D * ) infile_ped->Get("BC4_ped");
-	double pedBC1, pedBC2, pedBC3, pedBC4;
-	pedBC1 = BC1_ped->GetXaxis()->GetBinCenter(BC1_ped->GetMaximumBin());
-	pedBC2 = BC2_ped->GetXaxis()->GetBinCenter(BC2_ped->GetMaximumBin());
-	pedBC3 = BC3_ped->GetXaxis()->GetBinCenter(BC3_ped->GetMaximumBin());
-	pedBC4 = BC4_ped->GetXaxis()->GetBinCenter(BC4_ped->GetMaximumBin());
-	infile_ped->Close();
-	
-	// Now we can read in the digi file and look at BC1, BC2, BC3, BC4 correlation with pedestal subtracted!
-	TFile * inFile = NULL;
-	inFile = new TFile(argv[1]);
-	if (!inFile){
-		cerr << "Could not open file " << argv[1] <<"\n"
-			<< "\tBailing out\n";
-		return -2;
-	}
-	else{
-		//cerr << "Successfully opened file " << argv[1] << " and saved it to address " << inFile << "\n";
-	}
-	TTree * intree = (TTree *) inFile->Get("cbmsim");
-
-	// Setup the tree to read in the counters
-	TClonesArray * bc1Data 	= new TClonesArray("BmnTrigWaveDigit");
-	TClonesArray * bc2Data	= new TClonesArray("BmnTrigWaveDigit");
-	TClonesArray * bc3Data 	= new TClonesArray("BmnTrigWaveDigit");
-	TClonesArray * bc4Data	= new TClonesArray("BmnTrigWaveDigit");
-	TClonesArray * t0Data	= new TClonesArray("BmnTrigDigit");
-	
-	const int nEvents = intree->GetEntries();
-	// TQDC Branches
-	intree->SetBranchAddress("TQDC_BC1"	,&bc1Data);
-	intree->SetBranchAddress("TQDC_T0"	,&bc2Data);
-	intree->SetBranchAddress("TQDC_BC3"	,&bc3Data);
-	intree->SetBranchAddress("TQDC_BC4"	,&bc4Data);
-	
-	// TDC Branches
-	intree->SetBranchAddress("T0"		,&t0Data);
-
-
-	// Setup an output file and tree
-	TString outName;
-	outName.Form("qualityCheck/pedSub_spectra_"+run_number+".root");
-	TFile * outFile = new TFile(outName,"RECREATE");
-	TTree * outTree = new TTree("events","");
-	double adcBC1, adcBC2, adcBC3, adcBC4;
-	double tBC1, tBC2, tBC3, tBC4;
-	outTree->Branch("adcBC1"	,&adcBC1	,"adcBC1/D");
-	outTree->Branch("adcBC2"	,&adcBC2	,"adcBC2/D");
-	outTree->Branch("adcBC3"	,&adcBC3	,"adcBC3/D");
-	outTree->Branch("adcBC4"	,&adcBC4	,"adcBC4/D");
-	outTree->Branch("tBC1"		,&tBC1		,"tBC1/D");
-	outTree->Branch("tBC2"		,&tBC2		,"tBC2/D");
-	outTree->Branch("tBC3"		,&tBC3		,"tBC3/D");
-	outTree->Branch("tBC4"		,&tBC4		,"tBC4/D");
-
-
-	// Loop over events
-	int cntT0 = 0;
-	int cnt1 = 0;
-	int cnt2 = 0;
-	int cnt3 = 0;
-	int cnt4 = 0;
-
-	for (int event=0 ; event<nEvents ; event++)
-	{
-		//if (event % 1000== 0)
-			//cerr << "Working on event " << event << "\n";
-
-		intree->GetEvent(event);
+	while( std::getline(list,run_number) ){
+		cout << "Working on run: " << run_number << "\n";
 		
-		// Kill any event that doesn't have T0 entry, or that
-		// has more than 1 TDC in T0 -- cuts out 7% of data	
-		if( t0Data->GetEntriesFast() != 1) continue;
-		cntT0 ++;
-
-		// Do a DAQ counter
-		if( bc1Data->GetEntriesFast() > 0) cnt1 ++;
-		if( bc2Data->GetEntriesFast() > 0) cnt2 ++;
-		if( bc3Data->GetEntriesFast() > 0) cnt3 ++;
-		if( bc4Data->GetEntriesFast() > 0) cnt4 ++;
-
-		// Get the T0 Time
-		BmnTrigDigit * t0Signal = (BmnTrigDigit*) t0Data->At(0);
-		double t0Time = t0Signal->GetTime();
-
-		// For all the possible ADC inside of BC1, T0, I want to take
-		// the one that has the closest time to T0 from TDC, and use the
-		// others as the pedestal subtraction
-		if( bc1Data->GetEntriesFast() && bc2Data->GetEntriesFast() && bc3Data->GetEntriesFast() && bc4Data->GetEntriesFast() ){
-			int bc1Idx, bc2Idx, bc3Idx, bc4Idx;			
-
-				// Find the index for BC1
-			findIdx(bc1Data,bc1Idx,t0Time);
-			
-				// Find the index for BC2
-			findIdx(bc2Data,bc2Idx,t0Time);
-	
-				// Find the index for BC3
-			findIdx(bc3Data,bc3Idx,t0Time);
-	
-				// Find the index for BC4
-			findIdx(bc4Data,bc4Idx,t0Time);
-
-			// Now we use bc1Idx for actual analysis and use our pedestals
-			BmnTrigWaveDigit * signalBC1 = (BmnTrigWaveDigit*) bc1Data->At(bc1Idx);	
-			BmnTrigWaveDigit * signalBC2 = (BmnTrigWaveDigit*) bc2Data->At(bc2Idx);	
-			BmnTrigWaveDigit * signalBC3 = (BmnTrigWaveDigit*) bc3Data->At(bc3Idx);	
-			BmnTrigWaveDigit * signalBC4 = (BmnTrigWaveDigit*) bc4Data->At(bc4Idx);	
-
-			adcBC1 = signalBC1->GetPeak() - pedBC1;
-			adcBC2 = signalBC2->GetPeak() - pedBC2;
-			adcBC3 = signalBC3->GetPeak() - pedBC3;
-			adcBC4 = signalBC4->GetPeak() - pedBC4;
-			tBC1   = signalBC1->GetTime() - t0Time;			
-			tBC2   = signalBC2->GetTime() - t0Time;			
-			tBC3   = signalBC3->GetTime() - t0Time;			
-			tBC4   = signalBC4->GetTime() - t0Time;			
-
-			outTree->Fill();
+		TFile * infile = NULL;
+		
+		TString path = std::getenv("VMCWORKDIR");
+		path = path + "/build/bin/qualityCheck/checked_" + run_number + ".root";
+		
+		infile = new TFile(path);
+		if (!infile){
+			cerr << "Could not open file " << path <<"\n";
+			continue;
 		}
+
+		TVectorT<double> * ped		= (TVectorT<double>*)	infile->Get("ped");			
+		TVectorT<double> * tdcCnt	= (TVectorT<double>*)	infile->Get("tdcCnt");			
+		TVectorT<double> * t0Rate	= (TVectorT<double>*)	infile->Get("t0Rate");			
+		TVectorT<double> * tDiff	= (TVectorT<double>*)	infile->Get("tDiff");			
+		TVectorT<double> * tRes		= (TVectorT<double>*)	infile->Get("tRes");			
+		TVectorT<double> * pedTime	= (TVectorT<double>*)	infile->Get("pedTime");			
+		TVectorT<double> * carbonIn	= (TVectorT<double>*)	infile->Get("carbonIn");		
+		TVectorT<double> * carbonInWidth= (TVectorT<double>*)	infile->Get("carbonInWidth");	
+	
+		fileNo.push_back(  atof( run_number.c_str() ) );
+
+		rate_t0.push_back( (*t0Rate)[0] );
+
+		pedestals_bc1.push_back(	(*ped)[0]	);
+		pedestals_bc2.push_back(	(*ped)[1]	);
+		pedestals_bc3.push_back(	(*ped)[2]	);
+		pedestals_bc4.push_back(	(*ped)[3]	);
+
+		tdiff_bc1.push_back(	(*tDiff)[0]	);
+		tdiff_bc2.push_back(	(*tDiff)[1]	);
+		tdiff_bc3.push_back(	(*tDiff)[2]	);
+		tdiff_bc4.push_back(	(*tDiff)[3]	);
+
+		tres_bc1.push_back(	(*tRes)[0]	);
+		tres_bc2.push_back(	(*tRes)[1]	);
+		tres_bc3.push_back(	(*tRes)[2]	);
+		tres_bc4.push_back(	(*tRes)[3]	);
+
+		tped_bc1.push_back(	(*pedTime)[0]	);
+		tped_bc2.push_back(	(*pedTime)[1]	);
+		tped_bc3.push_back(	(*pedTime)[2]	);
+		tped_bc4.push_back(	(*pedTime)[3]	);
+
+		tdc_bc1.push_back(	(*tdcCnt)[0]	);
+		tdc_bc2.push_back(	(*tdcCnt)[1]	);
+		tdc_bc3.push_back(	(*tdcCnt)[2]	);
+		tdc_bc4.push_back(	(*tdcCnt)[3]	);
+
+		carbon_bc1.push_back(	(*carbonIn)[0]	);
+		carbon_bc2.push_back(	(*carbonIn)[1]	);
+
+		carbonWi_bc1.push_back(	(*carbonInWidth)[0]	);
+		carbonWi_bc2.push_back(	(*carbonInWidth)[1]	);
+
+		infile->Close();
 	}
-
-	// Get all the peaks for quality-control-check to see for significant drifts over time
-	TH1D * h = new TH1D("trash","trash",400,0,4000);
-	outTree->Draw("adcBC1>>trash");
-	double bc1Peak = h->GetXaxis()->GetBinCenter(h->GetMaximumBin());
-	outTree->Draw("adcBC2>>trash");
-	double bc2Peak = h->GetXaxis()->GetBinCenter(h->GetMaximumBin());
-	outTree->Draw("adcBC3>>trash");
-	double bc3Peak = h->GetXaxis()->GetBinCenter(h->GetMaximumBin());
-	outTree->Draw("adcBC4>>trash");
-	double bc4Peak = h->GetXaxis()->GetBinCenter(h->GetMaximumBin());
-
-	// Look at the BC1-BC2 carbon-in peak and the fraction of events in that peak vs total events
-	outTree->Draw("sqrt( adcBC1 * adcBC2 ) >>trash");
-	int s = h->GetXaxis()->GetBinCenter( h->GetMaximumBin() - 40);
-	int e = h->GetXaxis()->GetBinCenter( h->GetMaximumBin() + 40);
-	TFitResultPtr fit = h->Fit("gaus","QES","", s, e);
-	double mean = fit->Parameter(1);
-	double sig = fit->Parameter(2);
-	double maxX = mean + 1.*sig;
-	TFitResultPtr fit2 = h->Fit("gaus","QES","",0., maxX);
 	
-	// Let's look at beam quality:
-	int bmin = h->GetXaxis()->FindBin( fit2->Parameter(1) - 2*fit2->Parameter(2) );
-	int bmax = h->GetXaxis()->FindBin( fit2->Parameter(1) + 2*fit2->Parameter(2) );
-	double integral = h->Integral(bmin,bmax);
-	double beamQ = integral / nEvents;
+	int dim = fileNo.size();
+	TGraph * hT0Rate = new TGraph(dim , &fileNo[0] , &rate_t0[0] );
 
-	// Let's do BC3-BC4 carbon out -- only useful for no-target runs
-	outTree->Draw("sqrt( adcBC3 * adcBC4 ) >>trash");
-	TFitResultPtr fitO = h->Fit("gaus","QES",0,4000);
-	mean = fitO->Parameter(1);
-	sig = fitO->Parameter(2);
-	maxX = mean + 1.*sig;
-	TFitResultPtr fit2O = h->Fit("gaus","QES","",0., maxX);
+	TGraph * hPedBC1 = new TGraph(dim , &fileNo[0] , &pedestals_bc1[0] );
+	TGraph * hPedBC2 = new TGraph(dim , &fileNo[0] , &pedestals_bc2[0] );
+	TGraph * hPedBC3 = new TGraph(dim , &fileNo[0] , &pedestals_bc3[0] );
+	TGraph * hPedBC4 = new TGraph(dim , &fileNo[0] , &pedestals_bc4[0] );
 	
-	// Let's look at beam quality:
-	bmin = h->GetXaxis()->FindBin( fit2O->Parameter(1) - 2*fit2O->Parameter(2) );
-	bmax = h->GetXaxis()->FindBin( fit2O->Parameter(1) + 2*fit2O->Parameter(2) );
-	integral = h->Integral(bmin,bmax);
-	double beamQout = integral / nEvents;
+	TGraph * hTdcBC1 = new TGraph(dim , &fileNo[0] , &tdc_bc1[0] );
+	TGraph * hTdcBC2 = new TGraph(dim , &fileNo[0] , &tdc_bc2[0] );
+	TGraph * hTdcBC3 = new TGraph(dim , &fileNo[0] , &tdc_bc3[0] );
+	TGraph * hTdcBC4 = new TGraph(dim , &fileNo[0] , &tdc_bc4[0] );
+
+	std::vector<double> trash;
+	for( int i = 0 ; i < dim ; i++)
+		trash.push_back(0);
+
+	TGraphErrors * hTdiff_BC1 = new TGraphErrors(dim, &fileNo[0] , &tdiff_bc1[0] , &trash[0] , &tres_bc1[0] );
+	TGraphErrors * hTdiff_BC2 = new TGraphErrors(dim, &fileNo[0] , &tdiff_bc2[0] , &trash[0] , &tres_bc2[0] );
+	TGraphErrors * hTdiff_BC3 = new TGraphErrors(dim, &fileNo[0] , &tdiff_bc3[0] , &trash[0] , &tres_bc3[0] );
+	TGraphErrors * hTdiff_BC4 = new TGraphErrors(dim, &fileNo[0] , &tdiff_bc4[0] , &trash[0] , &tres_bc4[0] );
 	
-	// DAQ counters
-	double rateT0 = cntT0 / (double) nEvents;
-	double rate1  = cnt1 / (double) nEvents;
-	double rate2  = cnt2 / (double) nEvents;
-	double rate3  = cnt3 / (double) nEvents;
-	double rate4  = cnt4 / (double) nEvents;
+	TGraphErrors * hCarbonBC1 = new TGraphErrors(dim, &fileNo[0] , &carbon_bc1[0] , &trash[0] , &carbonWi_bc1[0]);
+	TGraphErrors * hCarbonBC2 = new TGraphErrors(dim, &fileNo[0] , &carbon_bc2[0] , &trash[0] , &carbonWi_bc2[0]);
+
+
+	TApplication theApp("App",&argc,argv);
 	
-	cout << "\n**********************************************\n";
-	cout << "\tPeak\tPedestal\tRate\n";
-	cout << "BC1:\t" << bc1Peak << "\t" << pedBC1 << "\t\t" << rate1 << "\n";
-	cout << "BC2:\t" << bc2Peak << "\t" << pedBC2 << "\t\t" << rate2 << "\n";
-	cout << "BC3:\t" << bc3Peak << "\t" << pedBC3 << "\t\t" << rate3 << "\n";
-	cout << "BC4:\t" << bc4Peak << "\t" << pedBC4 << "\t\t" << rate4 << "\n\n";
-	cout << "Carbon-In Peak (BC1-BC2):\t" << fit2->Parameter(1) << "\n\twidth of:\t\t" << fit2->Parameter(2) << "\n\tbeam quality:\t\t" << beamQ<< "\n\n";
-	cout << "Carbon-Out Peak (BC3-BC4):\t" << fit2O->Parameter(1) << "\n\twidth of:\t\t" << fit2O->Parameter(2) << "\n\tbeam quality:\t\t" << beamQout<< "\n\n";
-	cout << "T0 Rate:\t" << rateT0 << "\n";
-	cout << "**********************************************\n\n";
+	TCanvas * c1 = new TCanvas("c1");
+	hT0Rate->SetMarkerStyle(20);
+	hT0Rate->Draw("AP");
+	hT0Rate->GetXaxis()->SetTitle("Run Number");
+	hT0Rate->GetYaxis()->SetTitleOffset(1.5);
+	hT0Rate->GetYaxis()->SetRangeUser(0,1);
+	hT0Rate->GetYaxis()->SetTitle("Fraction");
+	hT0Rate->SetTitle("Fraction of Events where T0 has Single TDC");
+	c1->Update();
 	
-	string outname = string(argv[1]);
-	string init = std::getenv("VMCWORKDIR");
-	outname = init + "/build/bin/qualityCheck/checked-" + outname.substr(outname.find(".") - 9 , 4) + ".txt";
-	ofstream outfile(outname.c_str());
-	outfile << pedBC1 << "\t" << pedBC2 << "\t" << pedBC3 << "\t" << pedBC4 << "\n"
-		<< fit2->Parameter(1) << "\t" << fit2->Parameter(2) << "\t" << fit2O->Parameter(1) << "\t" << fit2O->Parameter(2) << "\n";
+	TCanvas * c2 = new TCanvas("c2");
+	c2->Divide(2,2);
+	c2->cd(1);
+	hPedBC1->SetMarkerStyle(20);
+	hPedBC1->Draw("AP");
+	hPedBC1->GetXaxis()->SetTitle("Run Number");
+	hPedBC1->GetYaxis()->SetTitleOffset(1.5);
+	hPedBC1->GetYaxis()->SetTitle("ADC [a.u]");
+	hPedBC1->SetTitle("Pedestal of BC1");
+	hPedBC1->GetYaxis()->SetRangeUser(40,140);
+	c2->cd(2);
+	hPedBC2->SetMarkerStyle(20);
+	hPedBC2->Draw("AP");
+	hPedBC2->GetXaxis()->SetTitle("Run Number");
+	hPedBC2->GetYaxis()->SetTitleOffset(1.5);
+	hPedBC2->GetYaxis()->SetTitle("ADC [a.u]");
+	hPedBC2->SetTitle("Pedestal of BC2");
+	hPedBC2->GetYaxis()->SetRangeUser(0,150);
+	c2->cd(3);
+	hPedBC3->SetMarkerStyle(20);
+	hPedBC3->Draw("AP");
+	hPedBC3->GetXaxis()->SetTitle("Run Number");
+	hPedBC3->GetYaxis()->SetTitleOffset(1.5);
+	hPedBC3->GetYaxis()->SetTitle("ADC [a.u]");
+	hPedBC3->SetTitle("Pedestal of BC3");
+	hPedBC3->GetYaxis()->SetRangeUser(-65,35);
+	c2->cd(4);
+	hPedBC4->SetMarkerStyle(20);
+	hPedBC4->Draw("AP");
+	hPedBC4->GetXaxis()->SetTitle("Run Number");
+	hPedBC4->GetYaxis()->SetTitleOffset(1.5);
+	hPedBC4->GetYaxis()->SetTitle("ADC [a.u]");
+	hPedBC4->SetTitle("Pedestal of BC4");
+	hPedBC4->GetYaxis()->SetRangeUser(100,200);
+	c2->Update();
 
-	outfile.close();
+
+	TCanvas * c3 = new TCanvas("c3");
+	c3->Divide(2,2);
+	c3->cd(1);
+	hTdcBC1->SetMarkerStyle(20);
+	hTdcBC1->Draw("AP");
+	hTdcBC1->GetXaxis()->SetTitle("Run Number");
+	hTdcBC1->GetYaxis()->SetTitleOffset(1.5);
+	hTdcBC1->GetYaxis()->SetTitle("Fraction");
+	hTdcBC1->SetTitle("BC1: Fraction of Events with only 1 TDC");
+	hTdcBC1->GetYaxis()->SetRangeUser(0,1);
+	c3->cd(2);
+	hTdcBC2->SetMarkerStyle(20);
+	hTdcBC2->Draw("AP");
+	hTdcBC2->GetXaxis()->SetTitle("Run Number");
+	hTdcBC2->GetYaxis()->SetTitleOffset(1.5);
+	hTdcBC2->GetYaxis()->SetTitle("Fraction");
+	hTdcBC2->SetTitle("BC2: Fraction of Events with only 1 TDC");
+	hTdcBC2->GetYaxis()->SetRangeUser(0,1);
+	c3->cd(3);
+	hTdcBC3->SetMarkerStyle(20);
+	hTdcBC3->Draw("AP");
+	hTdcBC3->GetXaxis()->SetTitle("Run Number");
+	hTdcBC3->GetYaxis()->SetTitleOffset(1.5);
+	hTdcBC3->GetYaxis()->SetTitle("Fraction");
+	hTdcBC3->SetTitle("BC3: Fraction of Events with only 1 TDC");
+	hTdcBC3->GetYaxis()->SetRangeUser(0,1);
+	c3->cd(4);
+	hTdcBC4->SetMarkerStyle(20);
+	hTdcBC4->Draw("AP");
+	hTdcBC4->GetXaxis()->SetTitle("Run Number");
+	hTdcBC4->GetYaxis()->SetTitleOffset(1.5);
+	hTdcBC4->GetYaxis()->SetTitle("Fraction");
+	hTdcBC4->SetTitle("BC4: Fraction of Events with only 1 TDC");
+	hTdcBC4->GetYaxis()->SetRangeUser(0,1);
+	c3->Update();
 
 
 
-	outTree->Write();
-	outFile->Close();
+	TCanvas * c4 = new TCanvas("c4");
+	c4->Divide(2,2);
+	c4->cd(1);
+	hTdiff_BC1->SetMarkerStyle(20);
+	hTdiff_BC1->Draw("AP");
+	hTdiff_BC1->GetXaxis()->SetTitle("Run Number");
+	hTdiff_BC1->GetYaxis()->SetTitleOffset(1.5);
+	hTdiff_BC1->GetYaxis()->SetTitle("T0 - BC1 Time [ns]");
+	hTdiff_BC1->SetTitle("Time Difference of BC1 with T0 and Resolution");
+	hTdiff_BC1->GetYaxis()->SetRangeUser(5.5,8.5);
+	c4->cd(2);
+	hTdiff_BC2->SetMarkerStyle(20);
+	hTdiff_BC2->Draw("AP");
+	hTdiff_BC2->GetXaxis()->SetTitle("Run Number");
+	hTdiff_BC2->GetYaxis()->SetTitleOffset(1.5);
+	hTdiff_BC2->GetYaxis()->SetTitle("T0 - BC2 Time [ns]");
+	hTdiff_BC2->SetTitle("Time Difference of BC2 with T0 and Resolution");
+	hTdiff_BC2->GetYaxis()->SetRangeUser(-31,-28);
+	c4->cd(3);
+	hTdiff_BC3->SetMarkerStyle(20);
+	hTdiff_BC3->Draw("AP");
+	hTdiff_BC3->GetXaxis()->SetTitle("Run Number");
+	hTdiff_BC3->GetYaxis()->SetTitleOffset(1.5);
+	hTdiff_BC3->GetYaxis()->SetTitle("T0 - BC3 Time [ns]");
+	hTdiff_BC3->SetTitle("Time Difference of BC3 with T0 and Resolution");
+	hTdiff_BC3->GetYaxis()->SetRangeUser(6.5,9.5);
+	c4->cd(4);
+	hTdiff_BC4->SetMarkerStyle(20);
+	hTdiff_BC4->Draw("AP");
+	hTdiff_BC4->GetXaxis()->SetTitle("Run Number");
+	hTdiff_BC4->GetYaxis()->SetTitleOffset(1.5);
+	hTdiff_BC4->GetYaxis()->SetTitle("T0 - BC4 Time [ns]");
+	hTdiff_BC4->SetTitle("Time Difference of BC4 with T0 and Resolution");
+	hTdiff_BC4->GetYaxis()->SetRangeUser(20,23);
+	c4->Update();
 
-	
+
+	TCanvas * c5 = new TCanvas("c5");
+	c5->Divide(2,1);
+	c5->cd(1);
+	hCarbonBC1->SetMarkerStyle(20);
+	hCarbonBC1->Draw("AP");
+	hCarbonBC1->GetXaxis()->SetTitle("Run Number");
+	hCarbonBC1->GetYaxis()->SetTitleOffset(1.5);
+	hCarbonBC1->GetYaxis()->SetTitle("ADC [a.u.]");
+	hCarbonBC1->SetTitle("Carbon Peak of BC1");
+	hCarbonBC1->GetYaxis()->SetRangeUser(900,1200);
+	c5->cd(2);
+	hCarbonBC2->SetMarkerStyle(20);
+	hCarbonBC2->Draw("AP");
+	hCarbonBC2->GetXaxis()->SetTitle("Run Number");
+	hCarbonBC2->GetYaxis()->SetTitleOffset(1.5);
+	hCarbonBC2->GetYaxis()->SetTitle("ADC [a.u.]");
+	hCarbonBC2->SetTitle("Carbon Peak of BC2");
+	hCarbonBC2->GetYaxis()->SetRangeUser(600,900);
+	c5->Update();
+
+
+	theApp.Run();
+
+	/*
+
+	*/
 
 	return 0;
 }
 
-void findIdx( TClonesArray* data, int &index , double refT){
-	double minT = 1e4;
-	for( int m = 0 ; m < data->GetEntriesFast() ; m++){
-		BmnTrigWaveDigit * signal = (BmnTrigWaveDigit*)data->At(m);
-		double time = fabs(signal->GetTime() - refT);
-		if( time < minT){
-			minT = time;
-			index = m;
-		}
-	}
-}
